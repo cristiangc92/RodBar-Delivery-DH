e28c5f2cf8e90be36d2e78fe393d2b6ecc6f0a0e
EBN10 <basilemir@gmail.com>
2023-11-04 11:55:31 -0300

    Servidor con Express creado con exito

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const express=require('express')
+const path=require('path')
+
+const app=express()
+
+const publicPath=path.resolve(__dirname,'../public')
+app.use(express.static(publicPath))
+
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,'view/home.html'))
+})
+
+app.listen(3001,()=>{
+    console.log('Servidor corriendo en el puerto 3001');
+})
