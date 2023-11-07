03b5a3d8433ec53a4ca08810dc248203da287247
unknown <cristiangc92@gmail.com>
2023-11-07 17:13:09 -0300

    Etiquetas semanticas

--- src/app.js
+++ src/app.js
@@ -1,15 +1,15 @@
-const express=require('express')
-const path=require('path')
+const express = require("express");
+const path = require("path");
 
-const app=express()
+const app = express();
 
-const publicPath=path.resolve(__dirname,'../public')
-app.use(express.static(publicPath))
+const publicPath = path.resolve(__dirname, "../public");
+app.use(express.static(publicPath));
 
-app.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname,'view/home.html'))
-})
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "view/index.html"));
+});
 
-app.listen(3001,()=>{
-    console.log('Servidor corriendo en el puerto 3001');
-})
+app.listen(3001, () => {
+  console.log("Servidor corriendo en el puerto 3001");
+});
